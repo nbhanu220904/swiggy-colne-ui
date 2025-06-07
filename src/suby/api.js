88b322e990636f7110c9af01485f56df19a43306
Nomula Bhanu Durga Prasad <159934816+nbhanu220904@users.noreply.github.com>
2025-06-07 22:10:22 +0530

    Update api.js

--- src/suby/api.js
+++ src/suby/api.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://swiggy-clone-backend-nodejs-ou96.onrender.com";
+export const API_URL = "https://swiggy-clone-backend-nodejs-ou96.onrender.com";
 
 // export const API_URL = "http://localhost:4000"
 
-export const API_URL = "https://backend-nodejs-suby.onrender.com";
+// export const API_URL = "https://backend-nodejs-suby.onrender.com";
